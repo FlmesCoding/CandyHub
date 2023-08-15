817cc9767ebacccdd9793c2067cbb169f2b0d29f
Flames <134011556+FlmesCoding@users.noreply.github.com>
2023-08-14 21:39:12 -0400

    Create gamelist.lua

---
+++ gamelist.lua
@@ -0,0 +1,28 @@
+if game.PlaceId == 142823291 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/MM2.lua'))()
+
+elseif game.PlaceId == 537413528 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/BABFT.lua'))()
+
+elseif game.PlaceId == 621129760 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/KAT.lua'))()
+
+elseif game.PlaceId == 662417684 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/LUCKY%20BLOCK.lua'))()
+
+elseif game.PlaceId == 8908228901 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/SHARKBITE.lua'))()
+
+elseif game.PlaceId == 1962086868 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/TOH.lua'))()
+
+elseif game.PlaceId == 6875469709 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/STRONGEST%20PUNCH.lua'))()
+
+elseif game.PlaceId == 3527629287 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/BIG%20PAINT%20BALL.lua'))()
+
+elseif game.PlaceId == 6403373529 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FlmesCoding/CandyHub/main/SLAP%20BATTLES.lua'))()
+
+end
